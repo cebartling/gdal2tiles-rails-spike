58028ecc2191f625ac45fc0aa33daedc7a757896
Christopher Bartling <chris.bartling@gmail.com>
2017-04-04 21:38:01 -0500

    Checkpoint.

--- app/controllers/mosaic_tile_controller.rb
+++ app/controllers/mosaic_tile_controller.rb
@@ -1,5 +1,5 @@
 class MosaicTileController < ApplicationController
   def create
-    GenerateMosaicTilesJob.perform_later()
+    GenerateMosaicTilesJob.set(queue: :tile_generation).perform_later()
   end
 end
